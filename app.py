3e5a8c75e89ba329526d769399c94db8651858d2
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-07-03 09:54:22 +0300

    Done

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+def solution(N):
+    result = []
     
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    
+    for k in range(26, 0, -1):
+        if N % k == 0:
+            repetitions = N // k
+            result = letters[:k] * repetitions
+            break
+    
+    return result
